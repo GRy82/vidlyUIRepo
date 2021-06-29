8b507eb7a83a17f4d1aefeb065c83e2fd9738efd
Gregory Ryan <gwryan@buffalo.edu>
2021-06-29 18:26:23 -0400

    Router changed to Route.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 //import logo from './logo.svg';
-import { Switch, Router, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import Movies from './components/movies';
 import Rentals from './components/rentals';
 import Customers from './components/customers';
+import NotFound from './components/NotFound';
 import NavBar from './components/navbar';
 
 function App() {
@@ -11,12 +12,11 @@ function App() {
     <main className='container'>
       <NavBar />
       <Switch>
-        <Router path='/movies' component={Movies} />
-        <Router path='/customers' component={Customers} />
-        <Router path='/rentals' component={Rentals} />
-        <Router path='/not-found' component={NotFound} />
+        <Route path='/movies' component={Movies} />
+        <Route path='/customers' component={Customers} />
+        <Route path='/rentals' component={Rentals} />
+        <Route path='/not-found' component={NotFound} />
         <Redirect to='/movies' from='/' />
-        <Router path='/' />
         <Redirect to='/not-found' />
       </Switch>
     </main>
